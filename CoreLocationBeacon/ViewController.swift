9bdc9cdf779b6fc8a62b898458420bd6c604120f
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-29 10:15:25 -0700

    Initial Commit

---
+++ CoreLocationBeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreLocationBeacon
+//
+//  Created by Prudhvi Gadiraju on 4/29/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
